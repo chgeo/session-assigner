513d90aad057bf1fce70182ec2b9481d06b15785
Christian Georgi <christian.georgi@sap.com>
2023-11-24 19:28:14 +0100

    Move test data

---
+++ srv/admin-service.js
@@ -0,0 +1,21 @@
+const cds = require('@sap/cds')
+
+class AdminService extends cds.ApplicationService { init(){
+
+  const { Sessions } = require('#cds-models/AdminService')
+
+  // auto-fill name w/ a unique value, just to ease testing
+  this.before ('CREATE', Sessions, autoFillID)
+
+
+  return super.init()
+}}
+
+function autoFillID(res) {
+  console.log('>>', res.data)
+  if (!res.data.ID) {
+    res.data.ID = 'session-' + cds.utils.uuid().split('-')[0]
+  }
+}
+
+module.exports = { AdminService }
